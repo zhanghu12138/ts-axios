255a4ce5b60b34dcce79fdf47bb9d898b0f0b64c
dajiandangdao <1330800769@qq.com>
2021-08-24 08:41:21 +0800

    响应数据

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,18 +1,27 @@
-import { AxiosRequestConfig } from './types/index'
-export default function xhr(config: AxiosRequestConfig): void {
-  const { data = null, url, method = 'get', headers } = config
+import { AxiosRequestConfig, AxiosPromise  } from './types/index'
+export default function xhr(config: AxiosRequestConfig): AxiosPromise{
+  return new Promise( resolve => {
+    const { data = null, url, method = 'get', headers, responseType } = config
 
-  const request = new XMLHttpRequest()
+    const request = new XMLHttpRequest()
+    if (responseType) request.responseType = responseType
+    request.open(method.toUpperCase(), url, true)
 
-  request.open(method.toUpperCase(), url, true)
-
-  Object.keys(headers).forEach(name => {
-    if (data === null && name.toLowerCase() === 'content-type') {
-      delete headers[name]
-    } else {
-      request.setRequestHeader(name, headers[name])
+    request.onreadystatechange = function handLoad() {
+      if(request.readyState !== 4) return
+      const responseHeaders = request.getResponseHeader
+      const responseData = re
     }
-  })
 
-  request.send(data)
+    Object.keys(headers).forEach(name => {
+      if (data === null && name.toLowerCase() === 'content-type') {
+        delete headers[name]
+      } else {
+        request.setRequestHeader(name, headers[name])
+      }
+    })
+
+    request.send(data)
+  })
+  
 }
